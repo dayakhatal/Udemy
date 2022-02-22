f8664a6544c22a5a9ec88685159b2e788dad0dd5
dayakhatal <dayakhatal@gmail.com>
2022-02-22 22:55:28 +0530

    *

---
+++ src/basicPractice/ReverseString.java
@@ -0,0 +1,31 @@
+package javaPractice;
+
+class ReverseString
+{
+	
+public static void main(String[] args) 
+{
+
+	
+	String ss="ayda";
+	
+	String rev="";
+	
+	int ll=ss.length();
+	
+	for(int i=ll-1;i>=0;i--)
+	{
+		
+		rev=rev+ss.charAt(i);
+		
+	}
+	
+	System.out.println(rev);
+}
+
+
+
+
+
+
+}
